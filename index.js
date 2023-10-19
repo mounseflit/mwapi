db20ab71285435d747db4cdb58c08defec0f108f
Mounsef Litniti <93012372+mounseflit@users.noreply.github.com>
2023-10-19 23:33:38 +0100

    hcc index.js

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
 const express = require("express");
 const { scrapeLogic } = require("./scrapeLogic");
 const app = express();
-
 const PORT = process.env.PORT || 4000;
-
 app.get("/scrape", (req, res) => {
   try {
-    let url = "https://waliye.men.gov.ma/moutamadris/Account";
-    let mail = "R130001518@taalim.ma";
-    let password = "130569Akram";
-    let year = "2022";
-    let round = "2";
-    const message = scrapeLogic(url, mail, password, year, round);
-    res.json({ message });
+   //const { mail, password } = req.body;
+        let mail = "R130001518@taalim.ma";
+        let password = "130569Akram";
+        let url = "https://waliye.men.gov.ma/moutamadris/Account";
+        let year = "2022";
+        let round = "2";
+        scrapeLogic(url, mail, password, year, round);
+        scrapeLogic(res);
   } catch {
     res.status(500).json({ error: "Failed to scrape data" });
   }
 });
-
 app.get("/", (req, res) => {
   res.send("Render Puppeteer server is up and running!");
 });
-
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
