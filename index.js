5631e71cfbe773cac581d42f779b2107c1e42b7c
Mounsef Litniti <93012372+mounseflit@users.noreply.github.com>
2023-10-19 04:15:49 +0100

    add request to index.js

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.get("/scrape", (req, res) => {
-//   try{
-//     const message = scrapeLogic("https://waliye.men.gov.ma/moutamadris/Account", mail, password, year, round);
-//     res.json({message})
-// }catch{
-//     console.error("Error in scraping", error);
-//     res.status(500).json({error:"Failed to scrape data"})
-// }
-  scrapeLogic(res);
+  try {
+   const { mail, password } = req.body;
+        let url = "https://waliye.men.gov.ma/moutamadris/Account";
+        let year = "2022";
+        let round = "2";
+        const message = scrapeLogic(url, mail, password, year, round);
+        res.json({ message });
+  } catch {
+    console.error("Error in scraping", error);
+    res.status(500).json({ error: "Failed to scrape data" });
+  }
 });
 
 app.get("/", (req, res) => {
