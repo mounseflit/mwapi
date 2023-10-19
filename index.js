c8e98455cdb19e06ec22d5d2cbe39c7bf95f29ed
Mounsef Litniti <93012372+mounseflit@users.noreply.github.com>
2023-10-19 05:17:10 +0100

    hc index.js

--- index.js
+++ index.js
@@ -4,13 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 app.get("/scrape", (req, res) => {
   try {
-   //const { mail, password } = req.body;
-        let mail = "R130001518@taalim.ma";
-        let password = "130569Akram";
-        let url = "https://waliye.men.gov.ma/moutamadris/Account";
-        let year = "2022";
-        let round = "2";
-        scrapeLogic(url, mail, password, year, round);
+        scrapeLogic(res);
   } catch {
     res.status(500).json({ error: "Failed to scrape data" });
   }
